0fc66fd2a462a71045bb1750dd98a2fd2bf32888
Marko Anastasov <marko@renderedtext.com>
2009-11-22 15:07:56 +0100

    Add option -M or --minimized to run the app minimized in tray.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <string>
 #include <gtkmm/main.h>
 #include "engine.hh"
 #include "main-window.hh"
@@ -9,7 +10,15 @@ main(int argc, char** argv)
 {
     Gio::init();
 
+    bool run_minimized = false;
+
     if (argc >= 2) {
+        std::string first_arg(argv[1]);
+        if (first_arg == "--minimized" || first_arg == "-M")
+            run_minimized = true;
+    }
+
+    if (argc >= 2 && !run_minimized) {
         Glib::ustring todays_path = tf::engine::init_todays_dir();
         std::cout << "cd " << todays_path << std::endl;
         for (int i = 0; i < argc-1; ++i) {
@@ -18,9 +27,13 @@ main(int argc, char** argv)
     } else {
         Gtk::Main kit(argc, argv);
         tf::MainWindow main_window;
+
         // By not attaching window to run(), hiding the window will not
         // invoke Gtk::main::quit() automatically.
-        main_window.show();
+
+        if (!run_minimized)
+            main_window.show();
+
         Gtk::Main::run();
     }
 
